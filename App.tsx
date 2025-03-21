5643f261e0f76d74d8f9420747cbfd8979e3b385
Matias Autalan <matias.autalan@tunubi.com>
2025-03-21 17:54:48 -0300

    add basic screens with mocks

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import  InstrumentsScreen  from './src/screens/Instruments/InstrumentsScreen';
+import PortfolioScreen from './src/screens/Portfolio/PortfolioScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarLabelStyle: { fontSize: 14 },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: { backgroundColor: 'white', height: 60 },
+            headerShown: true,
+          }}
+        >
+          <Tab.Screen name="Instrumentos" component={InstrumentsScreen} />
+          <Tab.Screen name="Portfolio" component={PortfolioScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
