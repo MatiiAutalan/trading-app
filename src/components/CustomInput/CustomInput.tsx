7822d7a77145ace5fca88c560543b10277784f56
Matias Autalan <matias.autalan@tunubi.com>
2025-03-25 09:47:36 -0300

    add customInput component

---
+++ src/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, TextInput, Switch } from 'react-native';
+import CustomSelect from '../CustomSelect/CustomSelect';
+import styles from './CustomInputStyles';
+
+type InputType = 'text' | 'number' | 'select' | 'toggle';
+
+export type InputConfig = {
+  type: InputType;
+  label: string;
+  key: string;
+  options?: string[];
+  defaultValue?: string | boolean | number;
+};
+
+type CustomInputProps = InputConfig & {
+  value: any;
+  onChange: (value: any) => void;
+};
+
+const CustomInput = ({
+  type,
+  label,
+  value,
+  onChange,
+  options,
+}: CustomInputProps) => {
+  const renderInput = () => {
+    switch (type) {
+      case 'text':
+      case 'number':
+        return (
+          <TextInput
+            style={styles.input}
+            placeholder={`Ingrese ${label.toLowerCase()}`}
+            keyboardType={type === 'number' ? 'numeric' : 'default'}
+            value={value?.toString() || ''}
+            onChangeText={onChange}
+          />
+        );
+      case 'select':
+        return (
+          <CustomSelect
+            options={options || []}
+            selectedValue={value || ''}
+            onValueChange={onChange}
+            placeholder={`Seleccione ${label.toLowerCase()}`}
+          />
+        );
+      case 'toggle':
+        return <Switch value={value || false} onValueChange={onChange} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <View style={styles.inputGroup}>
+      <Text style={styles.label}>{label}</Text>
+      {renderInput()}
+    </View>
+  );
+};
+
+export default CustomInput;
