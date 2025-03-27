8390e8192a809f6428d31281901d817a87660502
Matias Autalan <matias.autalan@tunubi.com>
2025-03-27 18:10:44 -0300

    add test to instrumentItem

--- src/components/InstrumentItem/InstrumentItem.tsx
+++ src/components/InstrumentItem/InstrumentItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Instrument } from '@types';
 import { calculateReturn } from '@utils';
 import styles from './InstrumentItemStyles';
